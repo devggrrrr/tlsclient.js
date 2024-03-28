f656e2afd029a7fa3efc1625ecab2895f1e9c5b2
ggrrrr6969 <67167703+ggrrrr6969@users.noreply.github.com>
2024-03-28 14:06:26 -0500

    ubuntu

--- scripts/tlspath.js
+++ scripts/tlspath.js
@@ -18,22 +18,7 @@ if (platform === "win32") {
   filename = "tls-client-linux";
   extension = "so";
 
-  let releaseDetails = fs.readFileSync("/etc/os-release", "utf8");
-  const lines = releaseDetails.split("\n");
-  const release = {};
-  lines.forEach((line, _) => {
-    // Split the line into an array of words delimited by '='
-    const words = line.split("=");
-    release[words[0].trim().toLowerCase()] = words[1].trim();
-  });
-
-  if (release.id.toLowerCase().includes("ubuntu")) {
-    distribution = "ubuntu-amd64";
-  } else if (release.id.toLowerCase().includes("alpine")) {
-    distribution = `alpine-amd64`;
-  } else {
-    distribution = arch == "arm64" ? arch : "armv7";
-  }
+  distribution = "ubuntu-amd64";
 } else {
   console.error(`Unsupported platform: ${platform}`);
   process.exit(1);
